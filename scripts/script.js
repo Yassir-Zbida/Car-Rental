efa299639fd95e29d4a9a2661df4a0fe91901d99
Yassir Zbida <zbidayassir10@gmail.com>
2024-12-08 13:08:41 +0100

    add new client modal with js function

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,25 @@
+// dashbord sidebzar hide and show function
+
 const sidebarIcon = document.getElementById('sidebarIcon');
 const sidebar = document.getElementById('sidebar');
 
 sidebarIcon.addEventListener('click', () => {
    sidebar.classList.toggle('hidden');
+});
+
+// add new client modal hide and show 
+const closeAddClientButtons = document.querySelectorAll('.closeAddClient');
+const addClientModal = document.getElementById('addClient');
+
+closeAddClientButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    console.log('Modal is being hidden');
+    addClientModal.classList.add('hidden');
+  });
+});
+
+// add new client button 
+const addClientBtn = document.getElementById('addClientBtn');
+addClientBtn.addEventListener('click', () => {
+   addClientModal.classList.remove('hidden');
 });
